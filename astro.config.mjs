8cec8472a32f4cb9a2423185392836a4b0bae814
Kaylee Wei <71412921+weikaylee@users.noreply.github.com>
2024-09-05 15:44:14 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://weikaylee.netlify.app',
+  base: 'my-repo',
   integrations: [mdx(), sitemap(), tailwind()]
 });
